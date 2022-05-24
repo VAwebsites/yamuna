c8dc109a301d3d932e4312fe09c77ebe4ccbc7a9
harshithva <harshith11032001@gmail.com>
2022-05-24 14:07:00 +0530

    done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+      <title>
+        @if (array_key_exists('TITLE', $_SERVER)) 
+        {{$_SERVER['TITLE']}}
+        @else
+        YAMUNA BACKEND
+        @endif
+    </title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
